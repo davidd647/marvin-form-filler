d03656fce0ce4c7f548af4d369031e36e8dd4e13
David Dales <david.dales@carpages.ca>
2018-07-24 12:24:02 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Marvin Form Filler
+
+A Google Chrome extension that fills all the inputs/selects/textareas in a form with quotes from Marvin the Paranoid Android!
+
+Just click Marvin to fill out all the things.
+
+### Reason for building this:
+- Whenever I go to auto-fill forms in Chrome with dummy text, there's always a field here and there that aren't touched. I don't want to auto-fill, and THEN go in to every field that was missed, wasting time entering dummy text... so I built this thing!
+
+### Outstanding issues:
+- Marvin is very depressed.
+
+### Directions:
+- Clone the repo into a directory
+- Go to chrome://extensions
+- Turn on Developer mode (there should be a toggle on the top right)
+- Click 'LOAD UNPACKED' 
+- Select the directory where you cloned the repo to
+- The extension should appear in your extensions list! Toggle it on
+- Go to a page with a form, and click Marvin!
---
+++ content.js
@@ -0,0 +1,44 @@
+// Icon source: Marvin Robot by Creaticca Creative Agency from the Noun Project
+
+let inputs = document.querySelectorAll('input')
+let selects = document.querySelectorAll('select')
+let textareas = document.querySelectorAll('textarea');
+
+function fillTheThings(){
+  inputs.forEach(input => {
+    switch( input.type ) {
+      case 'text':
+        input.value = 'sigh'
+        break
+      case 'checkbox':
+        input.checked = true
+        break
+      case 'email':
+        input.value = 'marvin@magrathea.com'
+        break
+      case 'tel':
+        input.value = '647 424 2424'
+        break
+      case 'number':
+        input.value = '42'
+        break
+    }
+  })
+
+  selects.forEach(select => {
+    select.selectedIndex = 1
+  })
+
+  textareas.forEach(textarea => {
+    textarea.value = `“What’s up?” asked Ford.
+    “I don’t know,” said Marvin, “I’ve never been there.”`
+  })
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      fillTheThings()
+    }
+  }
+);
